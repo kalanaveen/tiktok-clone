d90cb630dadf74c3c627a870839fbae5cd252fbb
naveen <kalanaveen55@gmail.com>
2021-05-23 17:32:49 +0530

    first commit

---
+++ src/components/VideoSidebar.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react'
+import './VideoSidebar.css';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import MessageIcon from '@material-ui/icons/Message';
+import ShareIcon from '@material-ui/icons/Share';
+import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
+function VideoSidebar({likes,messages,shares}) {
+    const [Liked, setLiked] = useState(false);
+    return (
+        <div className="videoSidebar">
+            <div className="videoSidebar__button">
+                {Liked ? (<FavoriteIcon fontSize="large" 
+                onClick={(e) => setLiked(false) }/>) : (
+                  <FavoriteBorderIcon fontSize="large"
+                  onClick={(e) => setLiked(true)}/>
+                )}
+                <p>{Liked ? likes + 1:likes}</p>
+            </div>
+            <div className="videoSidebar__button">
+                <MessageIcon fontSize="large" />
+                <p>{messages}</p>
+            </div>
+            <div className="videoSidebar__button">
+                <ShareIcon fontSize="large" />
+                <p>{shares}</p>
+            </div>
+        </div>
+    )
+}
+
+export default VideoSidebar
